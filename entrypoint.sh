27240d77af507d4b352acbf268e266c2a1755f90
Milad Nekofar <milad@nekofar.com>
2023-07-20 13:13:21 +0330

    refactor: update release flags-checking in `entrypoint.sh`
    This commit simplifies how different release flags (draft, prerelease, latest, and discussion_category) are checked and processed in the entrypoint.sh file. Instead of multiple if-else conditions, ternary operators are used to make the script shorter and cleaner. It also makes the flag-checking for the title, notes, and notes file more consistent.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,31 +6,21 @@ set -eu
 # Clear the GITHUB_TOKEN environment variable
 unset GITHUB_TOKEN
 
-# Check if both title and notes are not provided, then use --generate-notes flag
+# Check if both title, notes and notes file are not provided.
 generate_notes_flag=""
 if [[ -z "$INPUT_TITLE" && -z "$INPUT_NOTES" && -z "$INPUT_NOTES_FILE" ]]; then
-  generate_notes_flag="--generate-notes"
+ generate_notes_flag="--generate-notes"
 fi
 
-# Check if draft, prerelease, latest, and discussion_category are set to true or false
-draft_flag=""
-if [ "$INPUT_DRAFT" = "true" ]; then
-  draft_flag="--draft"
-fi
+# Check if draft, prerelease, and latest options are set to true.
+draft_flag=$([ "$INPUT_DRAFT" = "true" ] && echo "--draft" || echo "")
+prerelease_flag=$([ "$INPUT_PRERELEASE" = "true" ] && echo "--prerelease" || echo "")
+latest_flag=$([ "$INPUT_LATEST" = "true" ] && echo "--latest" || echo "")
 
-prerelease_flag=""
-if [ "$INPUT_PRERELEASE" = "true" ]; then
-  prerelease_flag="--prerelease"
-fi
-
-latest_flag=""
-if [ "$INPUT_LATEST" = "true" ]; then
-  latest_flag="--latest"
-fi
-
+# Check if discussion_category is provided.
 discussion_category_flag=""
 if [ -n "$INPUT_DISCUSSION_CATEGORY" ]; then
-  discussion_category_flag="--discussion-category $INPUT_DISCUSSION_CATEGORY"
+ discussion_category_flag="--discussion-category $INPUT_DISCUSSION_CATEGORY"
 fi
 
 # Use the provided token or GITHUB_TOKEN for authentication
@@ -39,18 +29,17 @@ gh auth login --with-token <<< "$INPUT_TOKEN"
 # Prepare the arguments for gh release create command
 arguments=()
 
-# Append --title flag if title is provided
+# Check if title is provided.
 if [ -n "$INPUT_TITLE" ]; then
-  arguments+=("--title" "$INPUT_TITLE")
+ arguments+=("--title" "$INPUT_TITLE")
 fi
 
-# Append --notes-file flag if notesFile is provided
+# Check if notes file is provided.
 if [ -n "$INPUT_NOTES_FILE" ]; then
-  arguments+=("--notes-file" "$INPUT_NOTES_FILE")
+ arguments+=("--notes-file" "$INPUT_NOTES_FILE")
 elif [ -n "$INPUT_NOTES" ]; then
-  # Append --notes flag with the provided release notes to the arguments
-  printf -v formatted_notes '%q' "$INPUT_NOTES"
-  arguments+=("--notes" "$formatted_notes")
+ printf -v formatted_notes '%q' "$INPUT_NOTES"
+ arguments+=("--notes" "$formatted_notes")
 fi
 
 # Append optional flags to the arguments array
